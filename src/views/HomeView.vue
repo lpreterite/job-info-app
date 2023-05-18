6ff567ab673f6e4770bf55fad16d0fb6998e0766
Packy <lpreterite@126.com>
2023-05-18 15:25:13 +0800

    增加ElementUI库

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+    <el-button type="primary" icon="el-icon-share"></el-button>
+    <el-button type="primary" icon="el-icon-delete"></el-button>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
   </div>
 </template>
 
